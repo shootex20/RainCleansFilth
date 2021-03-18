1c66c7b66004f54f89611f441b381a68ce91b379
chelseycoughlin <chelsey.coughlin@edu.sait.ca>
2021-03-18 03:25:01 -0600

    Updated Everything

--- RWF_Code.cs
+++ RWF_Code.cs
@@ -10,111 +10,101 @@ namespace RWF_Code
     [StaticConstructorOnStartup]
     internal static class RWF_Initializer
     {
+
+        public class FilthExtension : DefModExtension
+        {
+            public bool rainWashes = true;
+        }
+
         static RWF_Initializer()
         {
             LongEventHandler.QueueLongEvent(Setup, "LibraryStartup", false, null);
         }
+
+
         public static void Setup()
         {
-            //Loads ThingDef Name
-            ThingDef thingDef;
-            thingDef = ThingDef.Named("BaseFilth");
-            
+
             //Lists it all
-            List<ThingDef> ThingDefs = DefDatabase<ThingDef>.AllDefsListForReading;
-            for (int i = 0; i < ThingDefs.Count; i++)
+            List<ThingDef> allDefsListForReading = DefDatabase<ThingDef>.AllDefsListForReading;
+            allDefsListForReading.Where((ThingDef x) => ((Def)x).HasModExtension<FilthExtension>());
+
+
+             for (int i = 0; i < allDefsListForReading.Count; i++)
             {
-                if (ThingDefs[i].defName == "Filth_Dirt")
+
+                if (allDefsListForReading[i].defName == "Filth_AnimalFilth")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_Dirt;
-                    if (Controller.Settings.Filth_Dirt.Equals(true))
-                    {
-                        ThingDefs[i].filth.rainWashes = true;
-                    }
-                    break;
-                }
-                if (ThingDefs[i].defName == "Filth_AnimalFilth")
-                {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_AnimalFilth;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_AnimalFilth;
                     if (Controller.Settings.Filth_AnimalFilth.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "Filth_Trash")
+                if (((Def)allDefsListForReading[i]).defName == "Filth_Trash")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_Trash;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_Trash;
                     if (Controller.Settings.Filth_Trash.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "Filth_Sand")
+                if (((Def)allDefsListForReading[i]).defName == "Filth_Sand")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_Sand;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_Sand;
                     if (Controller.Settings.Filth_Sand.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "Filth_RubbleRock")
+                if (((Def)allDefsListForReading[i]).defName == "Filth_RubbleRock")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_RubbleRock;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_RubbleRock;
                     if (Controller.Settings.Filth_RubbleRock.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "Filth_RubbleBuilding")
+                if (((Def)allDefsListForReading[i]).defName == "Filth_RubbleBuilding")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_RubbleBuilding;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_RubbleBuilding;
                     if (Controller.Settings.Filth_RubbleBuilding.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "SlagRubble")
+                if (((Def)allDefsListForReading[i]).defName == "SlagRubble")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.SlagRubble;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.SlagRubble;
                     if (Controller.Settings.SlagRubble.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "SandbagRubble")
+                if (((Def)allDefsListForReading[i]).defName == "SandbagRubble")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.SandbagRubble;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.SandbagRubble;
                     if (Controller.Settings.SandbagRubble.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "Filth_MachineBits")
+                if (((Def)allDefsListForReading[i]).defName == "Filth_MachineBits")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_MachineBits;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_MachineBits;
                     if (Controller.Settings.Filth_MachineBits.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                if (ThingDefs[i].defName == "Filth_Ash")
+                if (((Def)allDefsListForReading[i]).defName == "Filth_Ash")
                 {
-                    ThingDefs[i].filth.rainWashes = Controller.Settings.Filth_Ash;
+                    allDefsListForReading[i].filth.rainWashes = Controller.Settings.Filth_Ash;
                     if (Controller.Settings.Filth_Ash.Equals(true))
                     {
-                        ThingDefs[i].filth.rainWashes = true;
+                        allDefsListForReading[i].filth.rainWashes = true;
                     }
-                    break;
                 }
-                
             }
         }
 
@@ -137,7 +127,6 @@ namespace RWF_Code
 
         public class Settings : ModSettings
         {
-            public bool Filth_Dirt = false;
             public bool Filth_AnimalFilth = false;
             public bool Filth_Trash = false;
             public bool Filth_Sand = false;
@@ -155,8 +144,6 @@ namespace RWF_Code
                 list.Begin(canvas);
                 list.Gap();
                 list.Label("RWF.Notes".Translate());
-                list.Gap();
-                list.CheckboxLabeled("RWF.FilthDirt".Translate(), ref Filth_Dirt);
                 list.Gap();
                 list.CheckboxLabeled("RWF.FilthAnimal".Translate(), ref Filth_AnimalFilth);
                 list.Gap();
@@ -184,17 +171,16 @@ namespace RWF_Code
             public override void ExposeData()
             {
 
-                    Scribe_Values.Look(ref Filth_Dirt, "rainWashesDirt", false);
-                    Scribe_Values.Look(ref Filth_AnimalFilth, "rainWashesAnimalFilth", false);
-                    Scribe_Values.Look(ref Filth_Trash, "rainWashesTrash", false);
-                    Scribe_Values.Look(ref Filth_Sand, "rainWashesSand", false);
-                    Scribe_Values.Look(ref Filth_RubbleRock, "rainWashesRubbleRock", false);
-                    Scribe_Values.Look(ref Filth_RubbleBuilding, "rainWashesRubbleBuilding", false);
-                    Scribe_Values.Look(ref SlagRubble, "rainWashesSlagRubble", false);
-                    Scribe_Values.Look(ref SandbagRubble, "rainWashesSandBagRubble", false);
-                    Scribe_Values.Look(ref Filth_MachineBits, "rainWashesMachineBits", false);
-                    Scribe_Values.Look(ref Filth_Ash, "rainWashesFilthAsh", false);
-                    base.ExposeData();
+                Scribe_Values.Look(ref Filth_AnimalFilth, "rainWashesAnimalFilth", false);
+                Scribe_Values.Look(ref Filth_Trash, "rainWashesTrash", false);
+                Scribe_Values.Look(ref Filth_Sand, "rainWashesSand", false);
+                Scribe_Values.Look(ref Filth_RubbleRock, "rainWashesRubbleRock", false);
+                Scribe_Values.Look(ref Filth_RubbleBuilding, "rainWashesRubbleBuilding", false);
+                Scribe_Values.Look(ref SlagRubble, "rainWashesSlagRubble", false);
+                Scribe_Values.Look(ref SandbagRubble, "rainWashesSandBagRubble", false);
+                Scribe_Values.Look(ref Filth_MachineBits, "rainWashesMachineBits", false);
+                Scribe_Values.Look(ref Filth_Ash, "rainWashesFilthAsh", false);
+                base.ExposeData();
 
 
             }
